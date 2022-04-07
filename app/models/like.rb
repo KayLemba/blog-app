e778b5a063176da44ed235af2af86997238159a6
KayLemba <kaylemba@yahoo.com>
2022-04-07 11:33:00 +0200

    Add forms and enable comments

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :user
   belongs_to :post
 
-  after_create :update_likes_counter
+  after_save :update_likes_counter
 
   private
 
   def update_likes_counter
-    post.increment!(:Likes_Counter)
+    post.increment!(:likes_counter)
   end
 end
--- db/migrate/20220325141647_create_comments.rb
+++ db/migrate/20220325141647_create_comments.rb
@@ -1,8 +1,6 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :users, null: false, foriegn_key: true, index:true
-      
       t.text :text
 
       t.timestamps
