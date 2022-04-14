ce1d33a991d6beb16c4ff210c462ff6203f03e23
KayLemba <kaylemba@yahoo.com>
2022-04-14 18:44:45 +0200

    Add api endpoints for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
   root 'users#index'
-
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
@@ -13,4 +15,10 @@ Rails.application.routes.draw do
 
   resources :comments, only: [:destroy]
   resources :likes, only: [:destroy]
+
+  namespace :api, defaults: { format: :json } do
+    resources :posts, only: [:index] do
+      resources :comments, only: [:create, :index]
+    end
+  end
 end
